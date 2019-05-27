84d138a35eb8c46dad2269700d29d2464128aadb
Brian Zelip <bzelip@gmail.com>
2019-05-27 15:15:43 -0400

    TheNav.vue: Create component

---
+++ src/components/TheNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="mt3 container mx-auto">
+    <ul class="list-reset flex">
+      <li class="flex-auto center">
+        <a href>Schedule</a>
+      </li>
+      <li class="flex-auto center">
+        <a href>Location</a>
+      </li>
+      <li class="flex-auto center">
+        <a href>Lodging</a>
+      </li>
+      <li class="flex-auto center">
+        <a href>Wedding Party</a>
+      </li>
+      <li class="flex-auto center">
+        <a href>Registry</a>
+      </li>
+    </ul>
+  </nav>
+</template>
