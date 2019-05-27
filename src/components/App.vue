b11452a1f7538fc105d76d3e502b465763cdb2e5
Brian Zelip <bzelip@gmail.com>
2019-05-27 15:16:29 -0400

    App.vue: Register and render TheHeader, remove other content

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,71 +1,15 @@
 <template>
   <div>
-    <h1>Erin and Matt are getting married 🎉</h1>
-    <p>hello world</p>
-    <ul class="list-reset col-8 mx-auto">
-      <li>
-        <div class="h00 MrBedfort">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(MrBedfort)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 Italianno">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(Italianno)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 Tangerine">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(Tangerine)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 Parisienne">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(Parisienne)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 GreatVibes">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(GreatVibes)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 Niconne">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(Niconne)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 Satisfy">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(Satisfy)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 KaushanScript">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(KaushanScript)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-      <li>
-        <div class="h00 EuphoriaScript">
-          Erin &amp; Matt
-          <span class="h2 sans-serif">(EuphoriaScript)</span>
-        </div>
-        <div class="baskerville bold">Home | About | Logistics | Registry</div>
-      </li>
-    </ul>
+    <TheHeader></TheHeader>
   </div>
 </template>
+
+<script>
+import TheHeader from "./TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader
+  }
+};
+</script>
