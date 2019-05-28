90cbaebe5216128a0b54ed78e80bce59e98be949
Brian Zelip <bzelip@gmail.com>
2019-05-27 22:47:31 -0400

    TheNav: Make fixed nav bar on scroll, via w3schools 😮

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -34,3 +34,36 @@
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  mounted() {
+    // sticky header on scroll via
+    // https://www.w3schools.com/howto/howto_js_sticky_header.asp
+    window.onscroll = function() {
+      myFunction();
+    };
+
+    var nav = document.querySelector("ul");
+    var sticky = nav.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        nav.classList.add("sticky");
+      } else {
+        nav.classList.remove("sticky");
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* sticky header on scroll via
+   https://www.w3schools.com/howto/howto_js_sticky_header.asp */
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 64rem;
+}
+</style>
