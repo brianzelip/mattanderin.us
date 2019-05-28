85dde0263065b341fb1f844bf77b9d3c402972e9
Brian Zelip <bzelip@gmail.com>
2019-05-27 21:45:49 -0400

    TheNav.vue: Display: block each <a> for larger surface area

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,19 +2,34 @@
   <nav class="mt3 container mx-auto">
     <ul class="list-reset flex">
       <li class="flex-auto center">
-        <a href>Schedule</a>
+        <a
+          class="block"
+          href
+        >Schedule</a>
       </li>
       <li class="flex-auto center">
-        <a href>Location</a>
+        <a
+          class="block"
+          href
+        >Location</a>
       </li>
       <li class="flex-auto center">
-        <a href>Lodging</a>
+        <a
+          class="block"
+          href
+        >Lodging</a>
       </li>
       <li class="flex-auto center">
-        <a href>Wedding Party</a>
+        <a
+          class="block"
+          href
+        >Wedding Party</a>
       </li>
       <li class="flex-auto center">
-        <a href>Registry</a>
+        <a
+          class="block"
+          href
+        >Registry</a>
       </li>
     </ul>
   </nav>
