10c8484e09a7dbbf155f72ee8e35c1e41906d244
Brian Zelip <bzelip@gmail.com>
2019-05-27 21:47:55 -0400

    TheHero.vue: Create component with fake background image

---
+++ src/components/TheHero.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    class="vh100"
+    id="hero"
+  >
+    <TheHeader></TheHeader>
+  </section>
+</template>
+
+<script>
+import TheHeader from "./TheHeader.vue";
+
+export default {
+  components: { TheHeader }
+};
+</script>
+
+
+<style>
+#hero {
+  /* "ANIMATION • Isabel & Carlos - Save the Date" by António Ferreira is licensed under CC BY-NC 4.0. https://www.behance.net/gallery/26803301/ANIMATION-Isabel-Carlos-Save-the-Date */
+  background-image: url(../img/couple.png);
+}
+</style>
