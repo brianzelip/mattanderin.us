a9682a071baf465a53683d4514b35a808c5e14d5
Brian Zelip <bzelip@gmail.com>
2019-08-04 15:35:37 -0400

    main.js: Swap in App for Splash

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 
-import Splash from './components/Splash.vue';
+import App from './components/App.vue';
 
 new Vue({
   el: '#app',
-  render: h => h(Splash)
+  render: h => h(App)
 });
