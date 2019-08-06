3f79c68a08d0992ba0f5cf563fc4b5184a038148
Brian Zelip <bzelip@gmail.com>
2019-08-06 12:07:20 -0400

    router: Create Honeymoon route w/ props

--- router.js
+++ router.js
@@ -1,12 +1,19 @@
 import VueRouter from 'vue-router';
 
 import TheHero from './components/TheHero.vue';
+import ThePage from './components/ThePage.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: TheHero
+  },
+  {
+    path: '/honeymoon',
+    name: 'honeymoon',
+    component: ThePage,
+    props: { page: 'TheHoneyMoon'}
   }
 ];
 
