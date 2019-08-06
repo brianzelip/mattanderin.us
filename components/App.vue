d96d41d801db67fce5158b47da0923895c2b174b
Brian Zelip <bzelip@gmail.com>
2019-08-06 07:34:40 -0400

    App: Playing around tryna figure it out

--- components/App.vue
+++ components/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <TheHero></TheHero>
+    <TheHeader></TheHeader>
+    <!-- <TheHero></TheHero> -->
     <section class="vh100"></section>
   </div>
 </template>
 
 <script>
+import TheHeader from "./TheHeader.vue";
 import TheHero from "./TheHero.vue";
 
 export default {
   components: {
+    TheHeader,
     TheHero
   }
 };
