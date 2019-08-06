d3861d67b176b2dcf1dd73714faa2c7fd478cda7
Brian Zelip <bzelip@gmail.com>
2019-08-06 12:08:20 -0400

    ThePage.vue: Create component

---
+++ components/ThePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="vh100">
+    <TheHeader></TheHeader>
+    <component :is="page" class="relative"></component>
+  </section>
+</template>
+
+<script>
+import TheHeader from './TheHeader.vue';
+import TheHoneyMoon from './TheHoneyMoon.vue';
+
+export default {
+  props: ['page'],
+  components: {
+    TheHeader,
+    TheHoneyMoon
+  }
+}
+</script>
