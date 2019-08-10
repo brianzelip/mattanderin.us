07e49c71c452340a1b2e6ecc54b4422ddbfc85af
Brian Zelip <bzelip@gmail.com>
2019-08-10 12:08:05 -0400

    TheBridalParty: Update markup

--- components/TheBridalParty.vue
+++ components/TheBridalParty.vue
@@ -1,22 +1,64 @@
-<template>
+ <template>
   <article class="vh100">
-    ***Bridesmaids***
-Maid of honor: Cassie Holden - Cass is my sister and very best friend in the world. We are only two years apart and have always been close. Or rather, we became close after we realized the futility of trying to best each other Tom-and-Jerry-style throughout our angsty childhood years. No one makes me laugh the way she does, (we call it the “ugly laughs”) and I challenge you to find a dance move that she can’t do. Except for breakdancing. She managed to break her foot playing badminton, so let’s not get too crazy.  
-
-Jessica Hadwin Marchand: Jessica and I have been friends since the fifth grade. From walking home together every day after school to being college roommates and everything in between, she’s been like a sister to me. When we weren’t at each others’ houses, we were on the phone talking about hilariously overdramatized concerns. Most of the time you could find us lying on a trampoline at my house, looking up at the stars, and laughing about all kinds of nonsense. That, or we were working on a collaborative piece of genius called “Mamma Like: A Soap Opera.” Basically, we were with each other through every devastatingly bad haircut and heartbreak from age 10 to 22. 
-
-Keshia Whitfield Norris: Keshia and I became fast friends in the 7th grade when we sat in the clarinet section together, which seemed to be the case from then until our senior year of high school. We had lots of sleepovers with prank phone calls, calling boys we liked, and things like that. My first ever drink was at her place - a splash of E&J Brandy in lemonade served in a Disney Jungle Book glass. Classy! Most of our memories revolve around band - going on trips and having lots of laughs. We were on the phone together all the time talking about the good times and being there for each other in the bad, even when we graduated from high school.
-
-Inkka Hudson Beaudion: Inkka, Keshia, Jessica, and I had a little crew going. All of us are still very connected. Even when we haven’t seen each other in years, we fall right back into our friendship, and I value that so much. I hardly get to see Inkka now that she lives in South Dakota where she owns a restaurant - Swamp Daddy’s, bringing some Cajun flavor to the Great White North. I’m extremely excited that she gets to come down for the wedding! We’ve been friends since the 8th grade and were in the clarinet section together in school, where she pretty much had me beat in chair placements every time. What we all wanted was to beat Leslie Smith. Damnit, Leslie Smith! Always first chair and shit... 
-Getting distracted here. Long story short: Inkka is the best, and this is the first time since her wedding over ten years ago that the three of us will all be together at the same time. I could cry. 
-
-I will cry. Okay, now I’m crying.
-
-Charly Bradford: Charly “Chucky Lee” Bradford and I met in grad school at UL. We were both having to commute from Pineville to Lafayette every morning at the time, so she opened her car door, said “Welcome to Prius!” and our friendship began. ***Fast forward***We became roommates, living in an apartment behind the Cracker Barrel, right next to a popular vagrant hangout spot littered with empty Bud Light and Redbull cans . Fast forward again to Wellness Wednesdays, a time when we tried to do ab workouts, just about died, decided to go for a walk, and ultimately ended up in my parents’ backyard smoking cigarettes. 
-
-Jess Hagar: 
-
-Abbie Sackmann-Zelip:
-
+    <div class="container">
+      <h1 class="center">Bridesmaids</h1>
+      <ul class="list-reset">
+        <li>
+          <h2>
+            Cassie Holden
+            <span class="italic">(Maid of Honor)</span>
+          </h2>
+          <p
+            class="h3"
+          >Cass is my sister and very best friend in the world. We are only two years apart and have always been close. Or rather, we became close after we realized the futility of trying to best each other Tom-and-Jerry-style throughout our angsty childhood years. No one makes me laugh the way she does, (we call it the "ugly laughs") and I challenge you to find a dance move that she can't do. Except for breakdancing. She managed to break her foot playing badminton, so let's not get too crazy.</p>
+          <img
+            class="circle"
+            src="../img/stock1.jpg"
+          />
+        </li>
+        <li>
+          <h2>Jessica Hadwin Marchand</h2>
+          <p
+            class="h3"
+          >Jessica and I have been friends since the fifth grade. From walking home together every day after school to being college roommates and everything in between, she's been like a sister to me. When we weren't at each others' houses, we were on the phone talking about hilariously overdramatized concerns. Most of the time you could find us lying on a trampoline at my house, looking up at the stars, and laughing about all kinds of nonsense. That, or we were working on a collaborative piece of genius called "Mamma Like: A Soap Opera." Basically, we were with each other through every devastatingly bad haircut and heartbreak from age 10 to 22.</p>
+        </li>
+        <li>
+          <h2>Keshia Whitfield Norris</h2>
+          <p
+            class="h3"
+          >Keshia and I became fast friends in the 7th grade when we sat in the clarinet section together, which seemed to be the case from then until our senior year of high school. We had lots of sleepovers with prank phone calls, calling boys we liked, and things like that. My first ever drink was at her place - a splash of E&J Brandy in lemonade served in a Disney Jungle Book glass. Classy! Most of our memories revolve around band - going on trips and having lots of laughs. We were on the phone together all the time talking about the good times and being there for each other in the bad, even when we graduated from high school.</p>
+        </li>
+        <li>
+          <h2>Inkka Hudson Beaudion</h2>
+          <p
+            class="h3"
+          >Inkka, Keshia, Jessica, and I had a little crew going. All of us are still very connected. Even when we haven't seen each other in years, we fall right back into our friendship, and I value that so much. I hardly get to see Inkka now that she lives in South Dakota where she owns a restaurant - Swamp Daddy's, bringing some Cajun flavor to the Great White North. I'm extremely excited that she gets to come down for the wedding! We've been friends since the 8th grade and were in the clarinet section together in school, where she pretty much had me beat in chair placements every time. What we all wanted was to beat Leslie Smith. Damnit, Leslie Smith! Always first chair and shit...</p>
+          <p
+            class="h3"
+          >Getting distracted here. Long story short: Inkka is the best, and this is the first time since her wedding over ten years ago that the three of us will all be together at the same time. I could cry. I will cry. Okay, now I'm crying.</p>
+        </li>
+        <li>
+          <h2>Charly Bradford</h2>
+          <p
+            class="h3"
+          >Charly "Chucky Lee" Bradford and I met in grad school at UL. We were both having to commute from Pineville to Lafayette every morning at the time, so she opened her car door, said "Welcome to Prius!" and our friendship began. ***Fast forward***We became roommates, living in an apartment behind the Cracker Barrel, right next to a popular vagrant hangout spot littered with empty Bud Light and Redbull cans . Fast forward again to Wellness Wednesdays, a time when we tried to do ab workouts, just about died, decided to go for a walk, and ultimately ended up in my parents' backyard smoking cigarettes.</p>
+        </li>
+        <li>
+          <h2>Jess Hagar</h2>
+          <p class="h3"></p>
+        </li>
+        <li>
+          <h2>Abbie Sackmann-Zelip</h2>
+          <p class="h3"></p>
+        </li>
+      </ul>
+    </div>
   </article>
 </template>
+
+<style scoped>
+/* img {
+  float: right;
+  shape-outside: circle();
+} */
+</style>
