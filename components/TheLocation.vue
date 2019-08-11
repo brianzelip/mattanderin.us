1a029baa4d5f45653b93e2a0565f82eb6c93db27
Brian Zelip <bzelip@gmail.com>
2019-08-11 08:23:08 -0400

    TheLocation.vue: Create component w/ embedded map

---
+++ components/TheLocation.vue
@@ -0,0 +1,11 @@
+<template>
+  <article class="vh100">
+    <div class="container">
+      <iframe
+        height="480"
+        src="https://www.google.com/maps/d/embed?mid=1tKvU6oM7yrNEbl4Hk-D7yz4O3Sw"
+        width="640"
+      ></iframe>
+    </div>
+  </article>
+</template>
