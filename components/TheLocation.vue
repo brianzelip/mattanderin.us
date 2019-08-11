565fcc84a9485fefc0c4ef39eda147be95bf7195
Brian Zelip <bzelip@gmail.com>
2019-08-11 08:45:06 -0400

    TheLocation: Update iframe styles and params

--- components/TheLocation.vue
+++ components/TheLocation.vue
@@ -1,11 +1,19 @@
 <template>
   <article class="vh100">
     <div class="container">
+      <h1 class="center">Location</h1>
       <iframe
-        height="480"
-        src="https://www.google.com/maps/d/embed?mid=1tKvU6oM7yrNEbl4Hk-D7yz4O3Sw"
-        width="640"
+        frameborder="0"
+        src="https://www.google.com/maps/d/embed?mid=1tKvU6oM7yrNEbl4Hk-D7yz4O3Sw&z=11"
       ></iframe>
     </div>
   </article>
 </template>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 75vh;
+  border: none;
+}
+</style>
