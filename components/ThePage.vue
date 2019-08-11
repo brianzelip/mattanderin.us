9a628f03431bbf71b48ddf2a218bedea32537da3
Brian Zelip <bzelip@gmail.com>
2019-08-11 08:23:36 -0400

    ThePage: Register and use TheLocation

--- components/ThePage.vue
+++ components/ThePage.vue
@@ -1,21 +1,26 @@
 <template>
   <section class="vh100">
     <TheHeader></TheHeader>
-    <component :is="page" class="relative"></component>
+    <component
+      :is="page"
+      class="relative"
+    ></component>
   </section>
 </template>
 
 <script>
-import TheHeader from './TheHeader.vue';
-import TheHoneyMoon from './TheHoneyMoon.vue';
-import TheBridalParty from './TheBridalParty.vue';
+import TheHeader from "./TheHeader.vue";
+import TheHoneyMoon from "./TheHoneyMoon.vue";
+import TheBridalParty from "./TheBridalParty.vue";
+import TheLocation from "./TheLocation.vue";
 
 export default {
-  props: ['page'],
+  props: ["page"],
   components: {
     TheHeader,
     TheHoneyMoon,
-    TheBridalParty
+    TheBridalParty,
+    TheLocation
   }
-}
+};
 </script>
