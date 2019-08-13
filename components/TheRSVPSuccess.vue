9a70e88557c5bbbf955214d91da6b73f14a07f62
Brian Zelip <bzelip@gmail.com>
2019-08-13 06:49:48 -0400

    RSVP fail/success: Update markup

--- components/TheRSVPSuccess.vue
+++ components/TheRSVPSuccess.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>SUCCESS!</h1>
+  <article class="vh100">
+    <div class="container">
+      <h1 class="mt0 pt2 h2 center">Thanks for responding, we can't wait to see you 🎉</h1>
+    </div>
+  </article>
 </template>
