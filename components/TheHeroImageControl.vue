efc5046f449d1119f53b7427093ac01dacdb9429
Brian Zelip <bzelip@gmail.com>
2019-08-14 07:29:22 -0400

    HeroImageControl: Emit custom event to parent on click

--- components/TheHeroImageControl.vue
+++ components/TheHeroImageControl.vue
@@ -1,9 +1,25 @@
 <template>
   <aside>
-    <section class="mr1">⬅️</section>
-    <section>➡️</section>
+    <section
+      class="mr1"
+      v-on:click="left"
+    >⬅️</section>
+    <section v-on:click="right">➡️</section>
   </aside>
 </template>
+
+<script>
+export default {
+  methods: {
+    left() {
+      this.$emit("left");
+    },
+    right() {
+      this.$emit("right");
+    }
+  }
+};
+</script>
 
 <style scoped>
 aside {
