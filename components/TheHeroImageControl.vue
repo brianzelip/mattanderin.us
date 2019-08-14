2318dd064ef591bc9dd44131c0740b0f0000d89a
Brian Zelip <bzelip@gmail.com>
2019-08-13 21:24:33 -0400

    TheHeroImageControl.vue: Create component

---
+++ components/TheHeroImageControl.vue
@@ -0,0 +1,21 @@
+<template>
+  <aside>
+    <section class="mr1">⬅️</section>
+    <section>➡️</section>
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+}
+
+section {
+  font-size: 2rem;
+}
+</style>
