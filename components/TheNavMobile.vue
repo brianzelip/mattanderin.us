66b482c0359e3959d21d3c30e816795f8b3893eb
Brian Zelip <bzelip@gmail.com>
2019-08-28 17:02:18 -0400

    TheNavMobile: Playing around with more designs

--- components/TheNavMobile.vue
+++ components/TheNavMobile.vue
@@ -1,46 +1,58 @@
 <template>
-  <nav class="px2 flex justify-center">
-    <div>
-      <button class="mt2 button">Menu</button>
-      <svg
-        viewBox="0 0 448 512"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+  <nav class="px2 flex justify-center relative">
+    <button class="mt2 button">
+      <!-- bars.svg -->
+      <!-- <svg viewBox="0 0 448 512">
         <path
           d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z"
         />
-      </svg>
-    </div>
+      </svg> -->
+      <!-- <svg viewBox="0 0 192 512"><path d="M96 152c39.8 0 72-32.2 72-72S135.8 8 96 8 24 40.2 24 80s32.2 72 72 72zm0-112c22.1 0 40 17.9 40 40s-17.9 40-40 40-40-17.9-40-40 17.9-40 40-40zm0 144c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm0 112c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40zm0 64c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm0 112c-22.1 0-40-17.9-40-40s17.9-40 40-40 40 17.9 40 40-17.9 40-40 40z"/></svg> -->
+      <!-- ellipsis-v.svg -->
+      <!-- <svg viewBox="0 0 64 512"><path d="M32 224c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zM0 136c0 17.7 14.3 32 32 32s32-14.3 32-32-14.3-32-32-32-32 14.3-32 32zm0 240c0 17.7 14.3 32 32 32s32-14.3 32-32-14.3-32-32-32-32 14.3-32 32z"/></svg> -->
+      <!-- caret-left.svg -->
+      <!-- <svg viewBox="0 0 192 512"><path d="M192 383.968v-255.93c0-28.425-34.488-42.767-54.627-22.627l-128 127.962c-12.496 12.496-12.497 32.758 0 45.255l128 127.968C157.472 426.695 192 412.45 192 383.968zM32 256l128-128v256L32 256z"/></svg> -->
+      <!-- chevron-left.svg -->
+      <svg viewBox="0 0 256 512"><path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"/></svg>
+      <span>Menu</span>
+    </button>
   </nav>
 </template>
 
 <style scoped>
-.button {
-  width: 50%;
+button {
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0.5rem;
+  line-height: 1;
+  z-index: 3;
   font-family: inherit;
   font-size: 0.875rem;
   font-weight: 400;
   text-decoration: none;
-  cursor: pointer;
-  display: inline-block;
-  line-height: 1;
-  padding: 0.5rem;
-  margin: 0;
+  cursor: default;
   height: auto;
-  border: none;
+  border: 1px solid rgba(0,0,0,.125);
   vertical-align: middle;
   -webkit-appearance: none;
-  color: var(--soft-black);
-  background-color: transparent;
+  color: var(--soft-white);
+  background-color: var(--soft-black);
 }
-.button:hover {
+
+button:hover {
   background-color: rgba(51, 34, 34, 0.4);
   color: var(--soft-white);
 }
 
 svg {
-  display: inline;
-  color: inherit;
-  width: 8px;
+  margin-right: .25rem;
+  width: .4rem;
+  fill: currentColor;
 }
 </style>
