6861e53c182022fa38dd280b33a16cf06631b581
Brian Zelip <bzelip@gmail.com>
2019-08-30 10:31:47 -0400

    TheHeader3, TheNavHeader3: Create new components for left vertical hero layout

---
+++ components/TheHeader3.vue
@@ -0,0 +1,90 @@
+<template>
+  <header
+    :style="{ marginBottom: marginBottom }"
+    class="p2"
+  >
+    <div class="flex-grow mr2">
+      <h1 class="m0 regular Italianno lh1">
+      Erin
+        <span class="mx2">&amp;</span> Matt
+      </h1>
+      <h2 class="mt0 regular lh1">
+        Winter Solstice &sdot; December 21
+        <span class="sup">st</span>, 2019 &sdot; Maurice, Louisiana
+      </h2>
+    </div>
+    <TheNavMobile class="md-hide"></TheNavMobile>
+    <TheNavHeader3
+      :currentPath="currentPath"
+      class="col-12 md-show"
+      v-on:add-margin-bottom="setMarginBottom"
+      v-on:reset-margin-bottom="resetMarginBottom"
+    ></TheNavHeader3>
+  </header>
+</template>
+
+<script>
+import TheHeaderH1 from "./TheHeaderH1.vue";
+import TheNavMobile from "./TheNavMobile.vue";
+import TheNav from "./TheNav.vue";
+import TheNavHeader3 from "./TheNavHeader3.vue";
+
+export default {
+  data() {
+    return {
+      marginBottom: "1rem"
+    };
+  },
+  props: ["currentPath"],
+  components: {
+    TheHeaderH1,
+    TheNavMobile,
+    TheNav,
+    TheNavHeader3
+  },
+  computed: {
+    isHome() {
+      return this.$route.name === "home";
+    }
+  },
+  methods: {
+    setMarginBottom(amount) {
+      this.marginBottom = `${amount}px`;
+    },
+    resetMarginBottom() {
+      this.marginBottom = `1rem`;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+h1 {
+  font-size: 3rem;
+}
+h2 {
+  font-size: 1rem;
+}
+
+@media (min-width: 40em) {
+  h1 {
+    font-size: 4rem;
+  }
+  h2 {
+    font-size: 1.25rem;
+  }
+}
+
+.sup {
+  font-size: 0.83em;
+  vertical-align: super;
+  margin-left: -0.3rem;
+}
+
+.container{
+  max-width:64rem;
+  margin-left:auto;
+  margin-right:auto;
+}
+</style>
---
+++ components/TheNavHeader3.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav class="flex justify-center">
+    <ul class="list-reset mb0">
+      <li
+        :key="i"
+        class=""
+        v-for="(page, i) in pages"
+      >
+        <router-link
+          :class="{ active: page.path === currentPath }"
+          :to="page.path"
+          class="button"
+        >{{ page.text }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["currentPath"],
+  data() {
+    return {
+      pages: [
+        { path: "/story", text: "Our Story" },
+        { path: "/schedule", text: "Schedule" },
+        { path: "/location", text: "Location" },
+        { path: "/party", text: "Wedding Party" },
+        { path: "/honeymoon", text: "Honeymoon Fund" },
+        { path: "/slideshow", text: "Slide Show" },
+        { path: "/rsvp", text: "RSVP" }
+      ]
+    };
+  },
+  mounted() {
+    // [sticky header on scroll via](https://www.w3schools.com/howto/howto_js_sticky_header.asp)
+    const nav = document.querySelector("nav");
+    const navHeight = nav.offsetHeight;
+    const navOffsetTop = nav.offsetTop;
+    const vm = this;
+
+    function stickyNav() {
+      if (window.pageYOffset > navOffsetTop) {
+        nav.classList.add("sticky");
+        // tell the parent to add bottom margin
+        // ps - 23 is a magic number, in fact, 23, 24, and 25 all work
+        vm.$emit("add-margin-bottom", navOffsetTop - navHeight + 23);
+      } else {
+        nav.classList.remove("sticky");
+        // tell the parent to reset bottom margin
+        vm.$emit("reset-margin-bottom");
+      }
+    }
+
+    window.onscroll = function() {
+      stickyNav();
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* sticky header on scroll via
+   https://www.w3schools.com/howto/howto_js_sticky_header.asp */
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: var(--soft-white);
+  z-index: 10;
+  box-shadow: 1px 2px 18px rgba(0, 0, 0, 0.1);
+}
+
+.button {
+  display: inline-block;
+  padding: .5rem;
+  text-decoration: none;
+}
+.button:hover {
+  background-color: rgba(51, 34, 34, 0.4);
+  color: var(--soft-white);
+}
+
+.active,
+.active:hover {
+  background-color: var(--soft-black);
+  color: var(--soft-white);
+  cursor: default;
+}
+</style>
