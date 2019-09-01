3ad4ad95a191769e14eab1a3f52c71a9d42b26a1
Brian Zelip <bzelip@gmail.com>
2019-09-01 07:52:11 -0400

    App.vue: Unregister components since  handles it all

--- components/App.vue
+++ components/App.vue
@@ -3,15 +3,3 @@
     <router-view></router-view>
   </div>
 </template>
-
-<script>
-import TheHeader from "./TheHeader.vue";
-import TheHero from "./TheHero.vue";
-
-export default {
-  components: {
-    TheHeader,
-    TheHero
-  }
-};
-</script>
