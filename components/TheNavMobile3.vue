121e74e032adaf1dbf7e8b9c5264376f6387dfec
Brian Zelip <bzelip@gmail.com>
2019-09-15 12:58:24 -0600

    TheNavMobile3.vue: Create component

---
+++ components/TheNavMobile3.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav>
+    <ul class="list-reset mb0">
+      <li
+        :key="i"
+        v-for="(page, i) in pages"
+      >
+        <router-link
+          :class="{active: page.path === currentPath}"
+          :to="page.path"
+          class="button"
+        >{{ page.text }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["color"],
+  data() {
+    return {
+      pages: [
+        { path: "/story", text: "Our Story" },
+        { path: "/schedule", text: "Schedule" },
+        { path: "/location", text: "Location" },
+        { path: "/party", text: "Wedding Party" },
+        { path: "/honeymoon", text: "Honeymoon Fund" },
+        { path: "/slideshow", text: "Slide Show" },
+        { path: "/rsvp", text: "RSVP" }
+      ]
+    };
+  },
+  computed: {
+    currentPath() {
+      return this.$route.path;
+    }
+  }
+};
+</script>
+
+<style scoped>
+nav {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 50%;
+  z-index: 10;
+  background-color: var(--soft-black);
+  padding: .5rem;
+  margin-right: calc(-50% - 1rem);
+}
+
+.button {
+  display: block;
+  padding: 0.5rem;
+  font-size: 1.25rem;
+  text-decoration: none;
+  color: var(--soft-white);
+}
+.button:hover {
+  background-color: var(--soft-white);
+  color: var(--soft-black);
+  transition: all 0.3s ease 0s;
+}
+
+.active,
+.active:hover {
+  background-color: var(--soft-black);
+  color: var(--soft-white);
+  cursor: default;
+}
+</style>
