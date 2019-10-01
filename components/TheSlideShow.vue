98593ab00b62a436a1b96a63eabfd825b4be83a8
Brian Zelip <bzelip@gmail.com>
2019-10-01 14:09:00 -0400

    TheSlideShow: Render images and file names POC via v-for

--- components/TheSlideShow.vue
+++ components/TheSlideShow.vue
@@ -1,6 +1,10 @@
 <template>
   <main>
     <h1 class="mt0 regular center pageTitle">Slide Show</h1>
+    <figure v-for="(image, index) in imagesEntries" :key="index">
+      <img :src="image[1]" :alt="image[0]" :title="image[0]" />
+      <figcaption>{{ image[0] }}</figcaption>
+    </figure>
   </main>
 </template>
 
