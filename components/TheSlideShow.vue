1a4c44f78ee4da4fcfd768a73a1e1243cb729bed
Brian Zelip <bzelip@gmail.com>
2019-10-01 14:06:18 -0400

    TheSlideShow: Import slideshow image paths via Parcel
    Import paths matching a glob via a hint in a Parcel issue,
    https://github.com/parcel-bundler/parcel/issues/1668

--- components/TheSlideShow.vue
+++ components/TheSlideShow.vue
@@ -3,3 +3,13 @@
     <h1 class="mt0 regular center pageTitle">Slide Show</h1>
   </main>
 </template>
+
+<script>
+import images from "../img/slideshow/*.jpg";
+
+export default {
+  created() {
+    console.log("images:::::::", Object.entries(images));
+  }
+};
+</script>
