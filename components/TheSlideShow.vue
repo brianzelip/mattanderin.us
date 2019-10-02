a5594b620be067daca5f1c1fd30018e8fade9733
Brian Zelip <bzelip@gmail.com>
2019-10-02 16:02:36 -0400

    TheSlideShow: Swap out button html entities for fontawesome icons, update styles accordingly

--- components/TheSlideShow.vue
+++ components/TheSlideShow.vue
@@ -7,8 +7,20 @@
         :src="activeImage.path"
         :title="activeImage.name"
       />
-      <button @click="goBack" class="prev">&lsaquo;</button>
-      <button @click="goForward" class="next">&rsaquo;</button>
+      <button @click="goBack" class="prev">
+        <svg viewBox="0 0 512 512">
+          <path
+            d="M504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zM256 472c-118.7 0-216-96.1-216-216 0-118.7 96.1-216 216-216 118.7 0 216 96.1 216 216 0 118.7-96.1 216-216 216zm-86.6-224.5l115.1-115c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L209 256l99.5 99.5c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0l-115.1-115c-4.6-4.8-4.6-12.4.1-17.1z"
+          />
+        </svg>
+      </button>
+      <button @click="goForward" class="next">
+        <svg viewBox="0 0 512 512">
+          <path
+            d="M8 256c0 137 111 248 248 248s248-111 248-248S393 8 256 8 8 119 8 256zM256 40c118.7 0 216 96.1 216 216 0 118.7-96.1 216-216 216-118.7 0-216-96.1-216-216 0-118.7 96.1-216 216-216zm86.6 224.5l-115.1 115c-4.7 4.7-12.3 4.7-17 0l-7.1-7.1c-4.7-4.7-4.7-12.3 0-17L303 256l-99.5-99.5c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0l115.1 115c4.6 4.8 4.6 12.4-.1 17.1z"
+          />
+        </svg>
+      </button>
       <figcaption>{{ activeImage.name }}</figcaption>
     </figure>
   </main>
@@ -94,21 +106,24 @@ img {
 button {
   position: absolute;
   align-self: center;
+  height: 2rem;
+  width: 2rem;
+  padding: 0;
   color: rgba(51, 34, 34, 0.5);
   border: none;
-  background-color: var(--soft-white);
-  height: 4rem;
-  width: 4rem;
+  background-color: transparent;
 }
 button:hover {
   color: var(--soft-black);
 }
 .prev {
   left: 0;
-  font-size: 4rem;
 }
 .next {
   right: 0;
-  font-size: 4rem;
+}
+svg {
+  fill: currentColor;
+  font-size: 2rem;
 }
 </style>
