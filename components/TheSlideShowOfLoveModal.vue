b831b883d8e5fbed21daf828b2d8f02b3cc10c80
Brian Zelip <bzelip@gmail.com>
2019-10-04 18:30:49 -0400

    TheSlideShowModal: Always be showing the love cursor

--- components/TheSlideShowOfLoveModal.vue
+++ components/TheSlideShowOfLoveModal.vue
@@ -6,8 +6,14 @@
         :src="activeImage.path"
         :title="activeImage.name"
       />
-      <button @click="goBack" class="prev">
-        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+      <button
+        @click="goBack"
+        class="prev"
+      >
+        <svg
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             class="control-background"
             d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm72.5 349.6a23.9 23.9 0 0 1 0 33.9l-17 17a23.9 23.9 0 0 1-33.9 0L142.1 273a24 24 0 0 1 0-34l135.5-135.5a23.9 23.9 0 0 1 33.9 0l17 17a23.9 23.9 0 0 1 0 33.9L226.9 256z"
@@ -18,7 +24,10 @@
           />
         </svg>
       </button>
-      <button @click="goForward" class="next">
+      <button
+        @click="goForward"
+        class="next"
+      >
         <svg viewBox="0 0 512 512">
           <path
             class="control-background"
@@ -30,7 +39,10 @@
           />
         </svg>
       </button>
-      <button class="control-close" @click="close">
+      <button
+        @click="close"
+        class="control-close"
+      >
         <svg viewBox="0 0 320 512">
           <path
             d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"
@@ -168,5 +180,11 @@ svg:hover .control-background,
   position: absolute;
   top: 0;
   right: 0;
+}
+
+section,
+button:hover {
+  cursor: url("../img/heart.png"), default;
+  transition: 200ms ease-in-out;
 }
 </style>
