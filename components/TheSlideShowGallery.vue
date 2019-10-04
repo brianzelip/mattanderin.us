ca8a9fa87fbff63b65ab34a07a622fe65ef2ef62
Brian Zelip <bzelip@gmail.com>
2019-10-04 06:18:08 -0400

    TheSlideShowGallery: Unheart title

--- components/TheSlideShowGallery.vue
+++ components/TheSlideShowGallery.vue
@@ -1,16 +1,6 @@
 <template>
   <main>
-    <h1 class="mt0 regular center pageTitle">
-      Slide Show of L<svg
-        class="heart"
-        viewBox="0 0 512 512"
-      >
-        <path
-          d="M462.3 62.7c-54.5-46.4-136-38.7-186.6 13.5L256 96.6l-19.7-20.3C195.5 34.1 113.2 8.7 49.7 62.7c-62.8 53.6-66.1 149.8-9.9 207.8l193.5 199.8c6.2 6.4 14.4 9.7 22.6 9.7 8.2 0 16.4-3.2 22.6-9.7L472 270.5c56.4-58 53.1-154.2-9.7-207.8zm-13.1 185.6L256.4 448.1 62.8 248.3c-38.4-39.6-46.4-115.1 7.7-161.2 54.8-46.8 119.2-12.9 142.8 11.5l42.7 44.1 42.7-44.1c23.2-24 88.2-58 142.8-11.5 54 46 46.1 121.5 7.7 161.2z"
-          fill="red"
-        />
-      </svg>ve
-    </h1>
+    <h1 class="mt0 regular center pageTitle">Slide Show of Love</h1>
     <ul
       class="list-reset"
       data-slide-container
@@ -85,9 +75,5 @@ export default {
 /* 2 columns wide */
 [data-grid-item].big {
   width: 50%;
-}
-.heart {
-  width: 1.5rem;
-  margin-right: 0.25rem;
 }
 </style>
