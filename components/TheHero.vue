6506f04dd26bf5d3cfa6dabf2231e3e8503749ea
Brian Zelip <bzelip@gmail.com>
2019-10-07 11:08:05 -0400

    TheHero: Ship .hero3 responsivity 🎉

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -11,7 +11,8 @@ export default {
       randomClass: "",
       heroColorMap: {
         hero1: "white",
-        hero2: "black"
+        hero2: "black",
+        hero3: "black"
       },
       images: [
         "splash01.jpg",
@@ -38,7 +39,7 @@ export default {
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
     } //via https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
-    const _class = `hero${rando(1, 2)}`;
+    const _class = `hero${rando(1, 3)}`;
     this.$set(this, "randomClass", _class);
     this.$emit("update-hero-color", this.heroColorMap[_class]);
   }
@@ -55,12 +56,17 @@ main {
   z-index: -1;
 }
 
-.hero1 {
+.hero1,
+.hero2,
+.hero3 {
   width: 100%;
   height: 100vh;
-  background-image: url(../img/splash04.jpg);
   background-size: cover;
   background-repeat: no-repeat;
+}
+
+.hero1 {
+  background-image: url(../img/splash04.jpg);
 }
 @media screen and (orientation: portrait) {
   .hero1 {
@@ -74,12 +80,24 @@ main {
 }
 
 .hero2 {
-  width: 100%;
-  height: 100vh;
   background-image: url(../img/splash06-flipped.jpg);
-  background-size: cover;
-  background-repeat: no-repeat;
   background-position: right 42% center;
+}
+
+.hero3 {
+  background-image: url(../img/splash07-flipped.jpg);
+}
+
+@media screen and (orientation: portrait) {
+  .hero3 {
+    background-position: bottom right 28%;
+  }
+}
+
+@media screen and (orientation: landscape) {
+  .hero3 {
+    background-position: right 63%;
+  }
 }
 
 .splash01 {
