7249c37dd81c9d167d08df6804d94d0e62f33dfc
Brian Zelip <bzelip@gmail.com>
2019-10-08 14:33:58 -0400

    TheOurStory: Refactor img with figure and caption responsively, add .container to main

--- components/TheOurStory.vue
+++ components/TheOurStory.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="container">
     <h1 class="mt0 regular center pageTitle">Our Story</h1>
     <p>
       It all started in a Griffin Hall computer room when Erin said, "Nice
@@ -8,8 +8,14 @@
       at the edges, kind of like he was an extra from Tombstone. Erin had to
       admit, though that, despite the alluring moustache, Matt was impolite at
       best. Lucky for him, he was quite cute, and Erin didn't forget that part.
-      <img src="../img/griffin-hall.jpg" class="griffin-hall" />
     </p>
+    <figure class="griffin-hall">
+      <img src="../img/griffin-hall.jpg" />
+      <figcaption>
+        Griffin Hall, University of Louisiana
+      </figcaption>
+    </figure>
+
     <p>
       Fast forward a few months: Erin is driving Matt and a van-full of friends
       when Matt &mdash; in a fit of passion &mdash; artfully grazes her leg,
@@ -46,59 +52,76 @@
       together again, and this time they weren't too awkward to suggest hanging
       out after the reading.
     </p>
+    <figure class="cafe-cottage">
+      <img src="../img/cafe-cottage.jpg" />
+      <figcaption>Cafe Cottage</figcaption>
+    </figure>
     <p>
       They went to the Wild Salmon (RIP). There was a loud band playing for a
       $10 cover, and they weren't very good... Suavely, Matt suggested they not
       go in there so he could be alone with Erin. They found themselves at Cafe
       Cottage &mdash; a den of iniquity filled with hipsters writing their first
       novel or creepers getting handsy at the bar, no in-between. They had
-      drinks and quesadillas while they talked about music and life.
-      <img src="../img/cafe-cottage.jpg" class="cafe-cottage" />
-      The two of them lived close to each other in Freetown near the university.
-      Like a two-minute walk close together. They ended up at Matt's place where
-      they had their first kiss, in his one-bedroom duplex apartment. It came to
-      be a place that they found themselves sitting on the porch, hidden behind
-      a huge banana tree, heckling young undergrads who liked to kick over
-      Matt's trash cans. It was the place where they got to know each other. Had
-      their first everything. It was a tiny place where cats and the occasional
-      possum would wander in, but it was pretty romantic to them.
+      drinks and quesadillas while they talked about music and life. The two of
+      them lived close to each other in Freetown near the university. Like a
+      two-minute walk close together. They ended up at Matt's place where they
+      had their first kiss, in his one-bedroom duplex apartment. It came to be a
+      place that they found themselves sitting on the porch, hidden behind a
+      huge banana tree, heckling young undergrads who liked to kick over Matt's
+      trash cans. It was the place where they got to know each other. Had their
+      first everything. It was a tiny place where cats and the occasional possum
+      would wander in, but it was pretty romantic to them.
     </p>
     <p>
       Seven years later, they find themselves planning a wedding, sitting in
       Matt's childhood home in Toledo. They are excited to be getting married in
       Maurice, Louisiana in a small venue called Woodlawn chapel on the Winter
-      Solstice. We hope you all can come and help us celebrate.
+      Solstice. We hope you all can come and help us celebrate!
     </p>
   </main>
 </template>
 
 <style scoped>
-.griffin-hall {
+figure {
   display: block;
-  margin: 0.5rem auto;
   max-width: 100%;
+  margin: 0.5rem 0;
+  padding: 0.5rem;
+  border: 2px solid var(--soft-black);
 }
+
+figure img {
+  width: 100%;
+  display: block;
+}
+
+figcaption {
+  margin-top: 0.5rem;
+  text-align: center;
+}
+
 @media screen and (min-width: 525px) {
-  .griffin-hall {
+  figure.griffin-hall {
     max-width: 500px;
-    max-height: 300px;
     float: right;
-    margin: 0;
-    padding: 0.5rem 0 0.5rem 0.5rem;
+    margin: 0.25rem 0 0 1rem;
+  }
+  figure.griffin-hall img {
+    max-height: 300px;
+    margin: 0 auto;
+  }
+}
+@media screen and (min-width: 960px) {
+  figure {
+    border-width: 4px;
   }
 }
 
-.cafe-cottage {
-  max-width: 100%;
-  margin: 0.5rem auto;
-  display: block;
-}
-@media screen and (min-width: 325px) {
-  .cafe-cottage {
-    max-width: 300px;
+@media screen and (min-width: 480px) {
+  figure.cafe-cottage {
+    max-width: 350px;
     float: left;
-    margin: 0;
-    padding: 0.5rem 0.75rem 0.5rem 0;
+    margin: 0.5rem 1rem 0 0;
   }
 }
 </style>
