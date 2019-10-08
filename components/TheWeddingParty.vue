71846b30c5c183bd309ae5562bcf7a4c7d0ca6a8
Brian Zelip <bzelip@gmail.com>
2019-10-08 15:33:36 -0400

    TheWeddingParty: Use html entities for better typography, style <br>, style and add a link

--- components/TheWeddingParty.vue
+++ components/TheWeddingParty.vue
@@ -11,10 +11,10 @@
               two years apart and have always been close. Or rather, we became
               close after we realized the futility of trying to best each other
               Tom-and-Jerry-style throughout our angsty childhood years. No one
-              makes me laugh the way she does, (we call it the "ugly laughs")
-              and I challenge you to find a dance move that she can't do. Except
-              for breakdancing. She managed to break her foot playing badminton,
-              so let's not get too crazy.
+              makes me laugh the way she does, (we call it the &ldquo;ugly
+              laughs&rdquo;) and I challenge you to find a dance move that she
+              can&rsquo;t do. Except for breakdancing. She managed to break her
+              foot playing badminton, so let&rsquo;s not get too crazy.
             </p>
             <figure class="m0 landscape">
               <img src="../img/Cass.jpg" />
@@ -27,15 +27,15 @@
             <p>
               Jessica and I have been friends since the fifth grade. From
               walking home together every day after school to being college
-              roommates and everything in between, she's been like a sister to
-              me. When we weren't at each others' houses, we were on the phone
-              talking about hilariously overdramatized concerns. Most of the
-              time you could find us lying on a trampoline at my house, looking
-              up at the stars, and laughing about all kinds of nonsense. That,
-              or we were working on a collaborative piece of genius called
-              "Mamma Like: A Soap Opera." Basically, we were with each other
-              through every devastatingly bad haircut and heartbreak from age 10
-              to 22.
+              roommates and everything in between, she&rsquo;s been like a
+              sister to me. When we weren&rsquo;t at each others&rsquo; houses,
+              we were on the phone talking about hilariously overdramatized
+              concerns. Most of the time you could find us lying on a trampoline
+              at my house, looking up at the stars, and laughing about all kinds
+              of nonsense. That, or we were working on a collaborative piece of
+              genius called &ldquo;Mamma Like: A Soap Opera.&rdquo; Basically,
+              we were with each other through every devastatingly bad haircut
+              and heartbreak from age 10 to 22.
             </p>
           </div>
         </li>
@@ -47,12 +47,13 @@
               the clarinet section together, which seemed to be the case from
               then until our senior year of high school. We had lots of
               sleepovers with prank phone calls, calling boys we liked, and
-              things like that. My first ever drink was at her place - a splash
-              of E&amp;J Brandy in lemonade served in a Disney Jungle Book
-              glass. Classy! Most of our memories revolve around band - going on
-              trips and having lots of laughs. We were on the phone together all
-              the time talking about the good times and being there for each
-              other in the bad, even when we graduated from high school.
+              things like that. My first ever drink was at her place&mdash;a
+              splash of E&amp;J Brandy in lemonade served in a Disney Jungle
+              Book glass. Classy! Most of our memories revolve around
+              band&mdash;going on trips and having lots of laughs. We were on
+              the phone together all the time talking about the good times and
+              being there for each other in the bad, even when we graduated from
+              high school.
             </p>
             <figure class="m0 square">
               <img src="../img/Keshia.jpg" />
@@ -64,20 +65,22 @@
           <div class="story">
             <p>
               Inkka, Keshia, Jessica, and I had a little crew going. All of us
-              are still very connected. Even when we haven't seen each other in
-              years, we fall right back into our friendship, and I value that so
-              much. I hardly get to see Inkka now that she lives in South Dakota
-              where she owns a restaurant - Swamp Daddy's, bringing some Cajun
-              flavor to the Great White North. I'm extremely excited that she
-              gets to come down for the wedding! We've been friends since the
-              8th grade and were in the clarinet section together in school,
-              where she pretty much had me beat in chair placements every time.
-              What we all wanted was to beat Leslie Smith. Damnit, Leslie Smith!
-              Always first chair and shit...
-              <br />Getting distracted here. Long story short: Inkka is the
-              best, and this is the first time since her wedding over ten years
-              ago that the three of us will all be together at the same time. I
-              could cry. I will cry. Okay, now I'm crying.
+              are still very connected. Even when we haven&rsquo;t seen each
+              other in years, we fall right back into our friendship, and I
+              value that so much. I hardly get to see Inkka now that she lives
+              in South Dakota where she owns a restaurant&mdash;<a
+                href="https://www.facebook.com/SwampDaddysCajunKitchen.SD"
+                >Swamp Daddy&rsquo;s Cajun Kitchen</a
+              >, bringing some Cajun flavor to the Great White North. I&rsquo;m
+              extremely excited that she gets to come down for the wedding!
+              We&rsquo;ve been friends since the 8th grade and were in the
+              clarinet section together in school, where she pretty much had me
+              beat in chair placements every time. What we all wanted was to
+              beat Leslie Smith. Damnit, Leslie Smith! Always first chair and
+              shit... <br />Getting distracted here. Long story short: Inkka is
+              the best, and this is the first time since her wedding over ten
+              years ago that the three of us will all be together at the same
+              time. I could cry. I will cry. Okay, now I&rsquo;m crying.
             </p>
             <figure class="m0 square">
               <img src="../img/Inkka.jpg" />
@@ -88,10 +91,10 @@
           <h2>Charly Bradford</h2>
           <div class="story">
             <p>
-              Charly "Chucky Lee" Bradford and I met in grad school at UL. We
-              were both having to commute from Pineville to Lafayette every
-              morning at the time, so she opened her car door, said "Welcome to
-              Prius!" and our friendship began.
+              Charly &ldquo;Chucky Lee&rdquo; Bradford and I met in grad school
+              at UL. We were both having to commute from Pineville to Lafayette
+              every morning at the time, so she opened her car door, said
+              &ldquo;Welcome to Prius!&rdquo; and our friendship began.
               <br />
               <i>Fast forward...</i>
               <br />We became roommates, living in an apartment behind the
@@ -101,7 +104,8 @@
               <i>Fast forward again...</i>
               <br />To Wellness Wednesdays, a time when we tried to do ab
               workouts, just about died, decided to go for a walk, and
-              ultimately ended up in my parents' backyard smoking cigarettes.
+              ultimately ended up in my parents&rsquo; backyard smoking
+              cigarettes.
             </p>
             <figure class="m0 square">
               <img src="../img/Charly.jpg" />
@@ -116,11 +120,12 @@
               better when we joined forces in a Dungeons and Dragons campaign.
               Those games quickly led to lots of Celine Dion and Mariah Carey
               songs being belted out on the back porch and lots of awkward
-              dancing to "Miss New Booty" and "That Butt Thang." Don’t worry.
-              Both of those songs are on the wedding playlist, and both of them
-              will have lengthy dedications to Jess that will both embarrass and
-              delight her. Love this girl and her husband Skip "I heard
-              everything you said" Shannon (also in the wedding). Good people.
+              dancing to &ldquo;Miss New Booty&rdquo; and &ldquo;That Butt
+              Thang.&rdquo; Don’t worry. Both of those songs are on the wedding
+              playlist, and both of them will have lengthy dedications to Jess
+              that will both embarrass and delight her. Love this girl and her
+              husband Skip &ldquo;I heard everything you said about me,
+              kids&rdquo; Shannon (also in the wedding). Good people.
             </p>
             <figure class="m0 portrait">
               <img src="../img/Jess.jpg" />
@@ -134,7 +139,7 @@
               Abbie, my soon-to-be sister-in-law, is a talented and fun person
               who I’ve so enjoyed getting to know since Matt and I got together
               seven years ago. Since then, she and her husband Brian have gone
-              on lots of adventures with us - camping in the 9th ward during
+              on lots of adventures with us&mdash;camping in the 9th ward during
               Mardi Gras, chasing chickens in Eunice, and hanging out in
               Baltimore where they now live. We’re both super excited that
               they’re spending a week with us in Louisiana!
@@ -151,19 +156,19 @@
       <h1 class="mt0 regular center pageTitle">The Dudes</h1>
       <ul class="list-reset mb0">
         <li class="pb2">
-          <h2>Mark "Bud" Sackmann</h2>
+          <h2>Mark &ldquo;Bud&rdquo; Sackmann</h2>
           <figure class="m0">
             <img src="../img/Bud.gif" />
           </figure>
         </li>
         <li class="pb2">
-          <h2>Chris "Wildcat" Sackmann</h2>
+          <h2>Chris &ldquo;Wildcat&rdquo; Sackmann</h2>
           <figure class="m0">
             <img src="../img/Chris.gif" />
           </figure>
         </li>
         <li class="pb2">
-          <h2>Brian "Brody Bro" Sackmann-Zelip</h2>
+          <h2>Brian &ldquo;Brody Bro&rdquo; Sackmann-Zelip</h2>
           <figure class="m0">
             <img src="../img/Brian.gif" />
           </figure>
@@ -175,7 +180,10 @@
           </figure>
         </li>
         <li class>
-          <h2>Skip "I heard everything you said about me, kids" Shannon</h2>
+          <h2>
+            Skip &ldquo;I heard everything you said about me, kids&rdquo;
+            Shannon
+          </h2>
           <figure class="m0">
             <img src="../img/Skip.gif" />
           </figure>
@@ -193,6 +201,11 @@
 
 .story p {
   order: 1;
+}
+
+.story br {
+  display: block;
+  margin-top: 1rem;
 }
 
 @media screen and (min-width: 1200px) {
@@ -228,5 +241,12 @@ hr {
   margin-top: 2rem;
   border: none;
   background-color: var(--soft-black);
+}
+
+a {
+  text-decoration: underline;
+}
+a:hover {
+  color: red;
 }
 </style>
