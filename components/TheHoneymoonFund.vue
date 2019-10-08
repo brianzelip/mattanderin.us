2fe3547a59b670ae8288105d256de75299f1627f
Brian Zelip <bzelip@gmail.com>
2019-10-08 14:32:44 -0400

    TheHoneyMoonFund: Move .container to main, prettier, delete typo

--- components/TheHoneymoonFund.vue
+++ components/TheHoneymoonFund.vue
@@ -1,9 +1,16 @@
 <template>
-  <main>
-    <div class="container">
-      <h1 class="mt0 regular center pageTitle">Honeymoon Fund</h1>
-      <p>If you know us at all, you know that we've been living in sin for quite some time. That means we don't really need much more than we've already acquired, so we are doing a Honeymoon Fund in lieu of gifts! If you want to contribute to it here, we would certainly appreciate it.</p>
-      <p>Right now, we are thinking of taking a cruise or flying to somewhere in the Caribbean where we can lay out on the beach for a while. Still searching for the best deal. It's in the works."</p>
-    </div>
+  <main class="container">
+    <h1 class="mt0 regular center pageTitle">Honeymoon Fund</h1>
+    <p>
+      If you know us at all, you know that we've been living in sin for quite
+      some time. That means we don't really need much more than we've already
+      acquired, so we are doing a Honeymoon Fund in lieu of gifts! If you want
+      to contribute to it here, we would certainly appreciate it.
+    </p>
+    <p>
+      Right now, we are thinking of taking a cruise or flying to somewhere in
+      the Caribbean where we can lay out on the beach for a while. Still
+      searching for the best deal. It's in the works.
+    </p>
   </main>
 </template>
