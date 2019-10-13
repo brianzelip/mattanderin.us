03f9f842cd4d2f3df95759cbd3299c090537e396
Brian Zelip <bzelip@gmail.com>
2019-10-13 06:58:41 -0400

    TheLocationmap: Abstract map iframe to its own component

---
+++ components/TheLocationMap.vue
@@ -0,0 +1,14 @@
+<template>
+  <iframe
+    frameborder="0"
+    src="https://www.google.com/maps/d/embed?mid=1tKvU6oM7yrNEbl4Hk-D7yz4O3Sw&z=11"
+  ></iframe>
+</template>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 75vh;
+  border: none;
+}
+</style>
