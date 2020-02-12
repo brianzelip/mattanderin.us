1f82bd355deee4e767ff39a12bd1d8f0241faaeb
Brian Zelip <bzelip@gmail.com>
2020-02-12 15:07:51 -0500

    refreshcomments.js: Write comments data to local file

--- refreshComments.js
+++ refreshComments.js
@@ -1,22 +1,24 @@
 'use strict';
 
+const fs = require('fs');
 const axios = require('axios').default;
-
 require('dotenv').config();
 
-const API = 'https://api.netlify.com/api/v1/';
+const url = 'https://api.netlify.com/api/v1/';
 const auth = `?access_token=${process.env.API_AUTH}`;
-const form = `/api/v1/forms/:form_id/submissions`;
-const sites = `/api/v1/sites`;
-const site_id = '8fdefce4-645a-42d5-8974-c63c6d695fa1';
 const form_id = '5e04cd60af1af50008ce205e';
+const API = `${url}/forms/${form_id}/submissions${auth}`;
 
 axios
-  .get(`${API}/forms/${form_id}/submissions${auth}`)
+  .get(`${API}`)
   .then(function(response) {
-    console.log(response.data);
-    // const site = response.data.filter(obj => obj.name === 'matt-and-erin');
-    // console.log('site_id is:::', site[0].site_id);
+    fs.writeFileSync(
+      './data/comments.json',
+      JSON.stringify(response.data, null, 2)
+    );
+    console.log(
+      `Comments data generated!\nThere are ${response.data.length} total comments.`
+    );
   })
   .catch(function(error) {
     console.log(error);
