50f0bba633ab817af96830fb4e4649f3ab5a0178
Brian Zelip <bzelip@protonmail.com>
2020-02-14 07:53:28 -0500

    main.js: Import and use new vue plugins

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueMeta from 'vue-meta';
+import Toasted from 'vue-toasted';
+import VueScrollTo from 'vue-scrollto';
 
 import router from './router.js';
 import App from './components/App.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueMeta);
+Vue.use(Toasted);
+Vue.use(VueScrollTo);
 
 new Vue({
   el: '#app',
