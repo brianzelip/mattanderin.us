9ad4f45ab22fe0abdb2e273673dd0f001f933a20
Brian Zelip <bzelip@protonmail.com>
2020-02-17 06:31:04 -0500

    submission-created.js: Add callback to handler

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -3,17 +3,20 @@
 const axios = require('axios').default;
 require('dotenv').config();
 
-const buildHook =
-  'https://api.netlify.com/build_hooks/5e49370b645e93bc77b1e743';
+const buildHook = `https://api.netlify.com/build_hooks/${process.env.HOOK_ID}`;
 const branch = 'render-comments';
 const title = 'triggered+by+submission-created+build+hook';
 const params = `?trigger_branch=${branch}&trigger_title=${title}`;
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
   axios
     .post(`${buildHook}${params}`, {})
     .then(function(response) {
       console.log(response);
+      callback(null, {
+        statusCode: 201,
+        body: 'Comment submitted!'
+      });
     })
     .catch(function(error) {
       console.log(error);
